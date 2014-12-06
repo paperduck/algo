639f4226ad6a319dffa21f1501fdf794f72b7273
Alan <kroner.alan@gmail.com>
2014-12-06 10:09:01 -0500

    Create main.h

---
+++ main/main.h
@@ -0,0 +1,16 @@
+/*
+PURPOSE:
+- Pull in custom algorithm libraries and run them. Load each into its own thread.
+- General maintenance such as connecting to broker, database connectivity, thread management.
+
+DATE CREATED:
+2014-12-06
+*/
+
+#include <stdio.h>
+
+int main ( int argc, const char * argv[] )
+{
+
+  return 0;
+}
