60d14f5fd27be9b031b8e3de6288546775ea2d06
user <user@deb.home>
2015-05-23 15:49:31 -0400

    minor changes

--- README.txt
+++ README.txt
@@ -1,10 +1,31 @@
-algo
-====
-BRIEF DESCRIPTION:
-Linux command-line algorithmic stock trading platform.
+# Project algo
 
-CREATED:
+### Introduction
+
+Linux command-line algorithmic trading engine.
+
+### Created
+
 2014-12-6
 
-STATUS:
+### Status
+
 In-progress
+
+### TODO
+
+1. Load historical data into MySQL database
+2. Backtest ideas until I find a successful one
+3. Add (free; no broker) forward testing to engine (real-time "dry run" paper trading)
+4. Forward-test until I find something successful
+5. Purchase:
+  - Account with broker
+  - Data feed
+  - API
+6. Implement forward testing using API
+  - Include toggle: paper trade or live trade
+7. Implement strategy using API
+8. Confirm that strategy still works when paper trade API
+9. Switch toggle, trade live
+
+
--- db/pidata_scripts/load_csv.sh
+++ db/pidata_scripts/load_csv.sh
@@ -1,24 +1,32 @@
-# Input Parameters:
-#
+# INPUT PARAMETERS
 # 	d
-# 		Directory that contains the data. Data could be in text files, csv, etc.
-# Remarks:
-#
+# 		Directory that contains the CSV files.
+#	tn_pre
+#		Table name prefix. 	
+# REMARKS
+#	Note that the CSV files from pi trading have the a 
+#	'.txt' extension.
+# EXAMPLES
+#	#> load_csv dji_ mydir/
 
 #!/bin/bash
-dir=$1
+tn_pre=$1
+dir=$2
 tn=""
+echo table name prefix: $tn_pre
 echo directory: $dir
-for f in $( ls $dir );
+for f in $( ls ${dir}/*.txt );
 do
-	# unzip if needed
-	dot_index = 
+	# derive table name
+	# Extract everything from file name up to extension
+	# convert to lowecase
+	#tn = ${f%.txt} | tr [:upper:] [:lower:]
 
-	if [ #
-	# derive table name from file name (conver to lowecase)
-	tn=${f%%.txt}
+	#tn=$( echo "${tn}" | tr [:upper:] [:lower:] ) # this syntax works
 
-	echo loading $tn
+	tn=$( echo ${f%.txt} | tr [:upper:] [:lower:] )
+	tn = "$tn_pre"_"$tn"
+	echo loading file $f into table $tn
 	# put data into database
 	echo ${mysql < call_algo_proc_test.mysql}
 	#mysql < algo_proc_load_pitrading_stocks ($dir/$f, $tn)
