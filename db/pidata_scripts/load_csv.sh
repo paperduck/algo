98257bc89287df722a155c81888c0d786aeca4c8
paperduck <paperduckgit@gmail.com>
2015-05-24 11:32:20 -0400

    bash calls mysql batch. Working on batch.

--- db/pidata_scripts/load_csv.sh
+++ db/pidata_scripts/load_csv.sh
@@ -1,8 +1,8 @@
 # INPUT PARAMETERS
-# 	dir_path
-# 		Directory that contains the CSV files.
-#	tn_pre
-#		Table name prefix. 	
+# 	dir_name    Name of directory that contains the CSV files.
+#	tn_pre      Table name prefix.
+#   db_user     Database username
+#   db_pass     Database password 	
 # REMARKS
 #	Note that the CSV files from pi trading have the a 
 #	'.txt' extension.
@@ -10,31 +10,50 @@
 #	#> load_csv dji_ mydir/
 
 #!/bin/bash
+
+# Verify arguments
 if [ -z "$1" ]; then
     echo 'missing directory path'
     exit 1
 fi
-dir_path=$1
+dir_name=$1
 if [ -z "$2" ]; then
     echo 'missing table name prefix'
     exit 1
 fi
 tn_pre=$2
+if [ -z "$3" ]; then
+    echo 'missing database username'
+    exit 1
+fi
+db_user=$3
+if [ -z "$4" ]; then
+    echo 'missing database password'
+    exit 1
+fi
+db_pass=$4
+
+pwd=$(pwd)
 tn=''
+full_file_path=''
 echo table name prefix: $tn_pre
-echo directory: $dir_path
-for f in $( ls ${dir_path} );
+echo directory: $dir_name
+for f in $( ls ${dir_name} );
 do
-    if [[ $f =~ [tT][xX][tT] ]] ; then
+    if [[ $f =~ \.txt$ ]] ; then
         echo file: $f
         # derive table name
         # Extract everything from file name up to extension
         # transform uppercase to lowercase
         tn=$( echo ${f%.txt} | tr [:upper:] [:lower:] ) # case sensitive
         tn=${tn_pre}_${tn}
-        echo loading file $f into table $tn
-        # put data into database
-    #	echo ${mysql < call_algo_proc_test.mysql}
+        full_file_path="${pwd}/${dir_name}/$f"
+        echo loading file $full_file_path into table $tn
+        # Call MySQL batch script to load CSV data into table
+        echo $(mysql -u${db_user} -p${db_pass} < call_test.mysql)
         #mysql < algo_proc_load_pitrading_stocks ($dir/$f, $tn)
+        echo
     fi
 done
+echo
+echo done
