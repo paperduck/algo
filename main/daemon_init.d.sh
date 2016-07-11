2764b589139565e25a22bcfc4d6a4fcc4faace84
paperduck <paperduckgit@gmail.com>
2016-07-11 23:35:01 +0900

    daemon init script

---
+++ main/daemon_init.d.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+#
+#
+
+### BEGIN INIT INFO
+# Provides:             daemon.sh
+# Required-Start:       
+# Required-Stop:        
+# X-Start-Before:   
+# Default-Start:        2 3 4 5
+# Default-Stop:         0 1 6
+# Short-Description: algo daemon
+# Description: algo daemon
+### END INIT INFO
+
+SUBJECT='Have a nice day.'
+BODY="
+$(who -r)
+
+Sincerely,
+  $(whoami)"
+
+case "$1" in
+  start)
+        python /home/user/raid/software_projects/algo/main/emailer.py "$SUBJECT" "$BODY"
+        ;;
+esac
+
+
+exit 0
